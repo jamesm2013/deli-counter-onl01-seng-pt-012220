ef6b885939af8d94a4c55d4a12a6b225fea07ff5
james Johnson <2013matthew2013@gmail.com>
2020-01-29 01:17:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line (katz_deli)
   if katz_deli.empty
     puts "the line is currently empty
     else"
-      current_sentence
+      current_sentence = "the is currently"
       counter = 1
       katz_deli.each do |p|
     puts "#{counter}. #{p}"
