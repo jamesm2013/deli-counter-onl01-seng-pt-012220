01e180ac92e039ef16913bbd5238993a2e8643d4
james Johnson <2013matthew2013@gmail.com>
2020-01-29 00:59:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,3 @@
 =require pry
-cBuild the line method that shows everyone their current place in the line. If there is nobody in line, it should say "The line is currently empty.".
-
-Build a method that a new customer will use when entering the deli. The take_a_number method should accept two arguments, the array for the current line of people (katz_deli), and a string containing the name of the person joining the end of the line. The method should call out (puts) the person's name along with their position in line. Top-Tip: Remember that people like to count from 1, not from 0 ("zero") like computers.
-
-l out (puts) that "There is nobody waiting 
 def line
 end
