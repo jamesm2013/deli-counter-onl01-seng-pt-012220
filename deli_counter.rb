d774998626d02133359e731b16ee9994f1bd4591
james Johnson <2013matthew2013@gmail.com>
2020-01-29 01:34:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,4 +17,4 @@ katz_deli << names
 puts "welcome, #{names}.you are number #{katz_deli} in line" 
 end
 def #now_serving(katz_deli)
-puts there is know one waiting to ne servered
+puts "there is know one waiting to ne servered"
